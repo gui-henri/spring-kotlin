11d26f3c52a7fd50ce2a951cfa45615086032c7c
Guilherme Henrique <guihenrique.dev@gmail.com>
2022-06-04 18:01:13 -0300

    Project Created

---
+++ src/main/kotlin/com/gui/backend/CursoSpringFlutterApplication.kt
@@ -0,0 +1,11 @@
+package com.gui.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CursoSpringFlutterApplication
+
+fun main(args: Array<String>) {
+	runApplication<CursoSpringFlutterApplication>(*args)
+}
---
+++ src/test/kotlin/com/gui/backend/CursoSpringFlutterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gui.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CursoSpringFlutterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
